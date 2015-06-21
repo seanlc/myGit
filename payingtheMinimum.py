7d90180c780a717ea3a442f955fd06224ec8f679
Sean Carstensen <seanlcarstensen@gmail.com>
2015-06-21 13:50:11 -0700

    payingtheMinimum

--- payingtheMinimum.py
+++ payingtheMinimum.py
@@ -1,16 +1,20 @@
-balance = 5000
+balance = 320000
 originalBalance = balance
-annualInterestRate = .098
+annualInterestRate = .2
 monthlyInterestRate = annualInterestRate / 12.0
-payment = 10
+hi = (originalBalance * (1+monthlyInterestRate) ** 12)/12.0
+lo = originalBalance / 12
 foundAnswer = False
 while not foundAnswer:
+    payment = (hi+lo)/2
     for n in range(12):
         balance -= payment
         balance += (balance * monthlyInterestRate)
-    if balance < 0:
+    if balance < 0 and balance > -.25:
         foundAnswer = True
+    elif balance < 0:
+        hi = payment
     else:
-        payment += 10
-        balance = originalBalance
-print ("Lowest Payment: " + str(payment))
+        lo = payment
+    balance = originalBalance
+print ("Lowest Payment: " + str(round(payment,2)))
