6a7941a8b9a824d4a870c47993a3b29f4fbd82b3
Sean Carstensen <seanlcarstensen@gmail.com>
2015-06-24 22:22:03 -0700

    updates

---
+++ palindromes.py
@@ -0,0 +1,19 @@
+def semordnilap(str1, str2):
+    '''
+    str1: a string, length > 1 and != str2
+    str2: a string, length > 1 and != str1
+    
+    
+    returns: True if str1 and str2 are semordnilap;
+             False otherwise.
+    '''
+    # Your code here
+    if len(str1) != len(str2):
+        return False
+    elif str1 == "" and str2 == "":
+        return True
+    else:
+        return str1[0] == str2[len(str2) - 1] and semordnilap(str1[1:],str2[:len(str2) - 1])
+str1 = "palindromes"
+str2 = "semordnilap"
+print(semordnilap(str1,str2))
